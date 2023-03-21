4797765a09793a4c384b06a7bf198a9b1cf04b45
Luan <ss.luan21@gmail.com>
2023-03-21 00:27:04 -0300

    API farmacia SpringBoot

---
+++ generationfarma/src/main/java/org/generation/generationfarma/GenerationfarmaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.generationfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationfarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationfarmaApplication.class, args);
+	}
+
+}
---
+++ generationfarma/src/test/java/org/generation/generationfarma/GenerationfarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.generationfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationfarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
